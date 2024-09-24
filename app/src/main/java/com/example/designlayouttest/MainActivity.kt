be3d758f58e9b43305f23e0eec7a6280d163d5bf
Simvader <simbadfernandes@gmail.com>
2024-09-23 22:04:52 -0300

    se borra un nuevo import

--- app/src/main/java/com/example/designlayouttest/MainActivity.kt
+++ app/src/main/java/com/example/designlayouttest/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.designlayouttest.ui.theme.AppTheme
