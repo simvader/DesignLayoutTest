3b607572bef8ff9e91fe28a22076f9fa46db34fc
Simvader <simbadfernandes@gmail.com>
2024-09-23 22:04:27 -0300

    se borran los imports no utilizados

--- app/src/main/java/com/example/designlayouttest/MainActivity.kt
+++ app/src/main/java/com/example/designlayouttest/MainActivity.kt
@@ -5,11 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
